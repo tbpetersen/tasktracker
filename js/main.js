62f9044e1f812ccb50f0270e1557ca1f918a8219
Paul Wilson <p4wilson@sdsc.edu>
2017-07-13 14:37:02 -0700

    Start of the code for the Filter Sidebar
    Built it and added some features.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+window.onload = function(){
+  openLeft();
+}
 
+function openLeft() {
+  document.getElementById("leftSidebar").style.display = "block";
+}
+
+function closeLeft() {
+  document.getElementById("leftSidebar").style.display = "none";
+}
