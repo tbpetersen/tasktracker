4913090bef309c158436b30fe23854a63d4d6a3f
Paul Wilson <p4wilson@sdsc.edu>
2017-07-13 16:31:34 -0700

    Increased size between pane and window

--- js/main.js
+++ js/main.js
@@ -2,16 +2,20 @@ window.onload = function(){
   openLeft();
 }
 
+/* Purpose: Open the filter pannel and move the screen with it.
+*/
 function openLeft() {
   var body = document.getElementById("main");
   var sideBar = document.getElementById("leftSidebar");
   var openButton = document.getElementById("leftOpenButton");
   sideBar.style.display = "block";
   sideBar.style.width = "10%";
-  body.style.marginLeft = "10%";
+  body.style.marginLeft = "11%";
   openButton.style.opacity = 0;
 }
 
+/* Purpose: Close the filter pannel and move the screen with it.
+*/
 function closeLeft() {
   var body = document.getElementById("main");
   var sideBar = document.getElementById("leftSidebar");
@@ -20,9 +24,4 @@ function closeLeft() {
   sideBar.style.width = "0%";
   body.style.marginLeft = "0%";
   openButton.style.opacity = 1;
-}
-
-function adjustScreen(){
-  var body = document.getElementById("body");
-  body.style.margin = "50%";
 }
