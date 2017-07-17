5d6e8a896edab97dda4449cb8f3b86cd9ec66d37
tbpetersen <tbpetersen@sdsc.edu>
2017-07-17 11:22:44 -0700

    Added functions for getting tokens and making api calls to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,123 @@
+const ZEN_AUTH_URL = "https://sdsc.zendesk.com/oauth/authorizations/new?response_type=token&client_id=client_services_tool_dev&scope=read%20write";
+const TRE_AUTH_URL = "https://trello.com/1/authorize?key=8886ef1a1bc5ad08caad020068a3f9a2&callback_method=fragment&return_url=https://localhost?trello";
 
+const ZEN_API_URL = "https://sdsc.zendesk.com/api/v2/";
+const TRE_API_URL = "https://trello.com/1/";
+
+const TRE_APP_KEY = "8886ef1a1bc5ad08caad020068a3f9a2";
+
+var zendeskToken = "";
+var trelloToken = "";
+var user;
+
+$(document).ready(function(){
+  /* This will redirect the user to trello and zendesk logins to get a token,
+  then get there user's data and log it to the console
+  getTokens();
+  zendeskGet("users/me.json").then(function(data){
+    console.log(data);
+    trelloGet("members/me").then(function(trelloData){
+      console.log(trelloData);
+    })
+  });
+  */
+});
+
+function saveTokenFromURL(){
+  saveTrelloTokenFromURL();
+  saveZendeskTokenFromURL();
+}
+
+function saveTrelloTokenFromURL(){
+  var url = window.location.href;
+  var tokenString = "#token=";
+
+  if(! url.includes(tokenString)){
+    return;
+  }
+
+  var tokenStart = url.indexOf(tokenString) + tokenString.length;
+  token = url.substring(tokenStart);
+  localStorage["trelloToken"] = token;
+}
+
+function saveZendeskTokenFromURL(){
+  var url = window.location.href;
+  var tokenString = "#access_token=";
+
+  if(! url.includes(tokenString)){
+    return;
+  }
+
+  var tokenStart = url.indexOf(tokenString) + tokenString.length;
+  var tokenEnd = url.indexOf("&scope=");
+  token = url.substring(tokenStart, tokenEnd);
+  localStorage["zendeskToken"] = token;
+}
+
+function getTokens(){
+    saveTokenFromURL();
+    getZendeskToken();
+    getTrelloToken();
+}
+
+function getZendeskToken(){
+  zendeskToken = localStorage.getItem("zendeskToken");
+  if(zendeskToken == undefined){
+    redirectToZendeskLogin();
+  }
+}
+
+function getTrelloToken(){
+  trelloToken = localStorage.getItem("trelloToken");
+  if(trelloToken == undefined){
+    redirectToTrelloLogin();
+  }
+}
+
+function redirectToTrelloLogin(){
+  window.location.href = TRE_AUTH_URL
+}
+
+function redirectToZendeskLogin(){
+  window.location.href = ZEN_AUTH_URL;
+}
+
+function getUser(){
+  return zendeskGet("users/me.json");
+}
+
+
+
+function zendeskGet(url){
+  return new Promise(function(resolve, reject){
+    $.ajax({
+    type: "GET",
+    beforeSend: function(request) {
+      request.setRequestHeader("Authorization", "Bearer " + zendeskToken);
+    },
+    url: ZEN_API_URL + url,
+    success: function(data){
+      resolve(data)
+    },
+    error: function(data){
+      reject(data)
+      }
+    });
+  });
+}
+
+function trelloGet(url){
+  return new Promise(function(resolve, reject){
+    $.ajax({
+    type: "GET",
+    url: TRE_API_URL + url + "?token=" + trelloToken + "&key=" + TRE_APP_KEY,
+    success: function(data){
+      resolve(data)
+    },
+    error: function(data){
+      reject(data)
+      }
+    });
+  });
+}
