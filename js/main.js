09dd79ccf22aba0e0700f3c8f5c26e54f5e547a6
Shiva Sharma <Shiva Sharma>
2017-07-18 12:56:47 -0700

    Basic table

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+var fixHelper = function(e, ui) {
+    ui.children().each(function() {
+        $(this).width($(this).width());
+    });
+    return ui;
+};
+
+$("#sort tbody").sortable({
+    helper: fixHelper
+}).disableSelection();
 
