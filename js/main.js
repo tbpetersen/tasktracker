77c73ff4c7cb0aa9ea4176fbbee7a732a8c742eb
Shiva Sharma <Shiva Sharma>
2017-07-18 14:15:23 -0700

    Table

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-var fixHelper = function(e, ui) {
+var maxWidth = 0;
+$('#table td:nth-child(3)').each(function(){
+    if(maxWidth < $(this).width())
+        maxWidth = $(this).width();
+});
+
+$('#table td:nth-child(3)').css('width',maxWidth);
+
+var fixHelperModified = function(e, tr) {
+    var $originals = tr.children();
+    var $helper = tr.clone();
+    $helper.children().each(function(index) {
+        $(this).width($originals.eq(index).width()+17); // 16 - 18
+    });
+    return $helper;
+},
+    updateIndex = function(e, ui) {
+        $('td.index', ui.item.parent()).each(function (i) {
+            $(this).html(i + 1);
+        });
+    };
+
+$("#table tbody").sortable({
+    helper: fixHelperModified,
+    stop: updateIndex
+}).disableSelection();
+/*var fixHelper = function(e, ui) {
     ui.children().each(function() {
         $(this).width($(this).width());
     });
@@ -8,4 +34,4 @@ var fixHelper = function(e, ui) {
 $("#sort tbody").sortable({
     helper: fixHelper
 }).disableSelection();
-
+*/
