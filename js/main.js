6347094e4dc86f511254c074784a49dd31aa7f38
Paul Wilson <p4wilson@sdsc.edu>
2017-07-21 12:37:22 -0700

    More functionality with sorting. Category and Last modified are now in.

--- js/main.js
+++ js/main.js
@@ -446,7 +446,6 @@ function refresh(){
 
 /*Sort the data alphabetically*/
 function sortAlphabet(){
-  console.log("Sorting alphabetically.");
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("table");
   switching = true;
@@ -465,8 +464,6 @@ function sortAlphabet(){
       one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[0];
       y = rows[i + 1].getElementsByTagName("TD")[0];
-      console.log(x);
-      console.log(y);
       //check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         //if so, mark as a switch and break the loop:
@@ -486,7 +483,6 @@ function sortAlphabet(){
 
 /*Sort the data alphabetically reversed*/
 function sortAlphabetReverse(){
-  console.log("Sorting alphabetically reverse.");
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("table");
   switching = true;
@@ -505,8 +501,6 @@ function sortAlphabetReverse(){
       one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[0];
       y = rows[i + 1].getElementsByTagName("TD")[0];
-      console.log(x);
-      console.log(y);
       //check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
         //if so, mark as a switch and break the loop:
@@ -525,20 +519,120 @@ function sortAlphabetReverse(){
 
 /*Sort the data by due date*/
 function sortDueDate(){
-  console.log("Sorting by due date.");
 
 }
 
 /*Sort the data by date*/
 function sortStartDate(){
-  console.log("Sorting Cronologically.");
 
 }
 
-/*Sort the data by status*/
-function sortStatus(){
-  console.log("Sorting by Status.");
+/*Sort the data by Category*/
+function sortCategory(){
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[3];
+      y = rows[i + 1].getElementsByTagName("TD")[3];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 
+/*Sort by the latest modified first*/
+function sortLastModified(){
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[2];
+      y = rows[i + 1].getElementsByTagName("TD")[2];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+/*Sort by the latest modified last*/
+function sortlastModifiedReversed(){
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[2];
+      y = rows[i + 1].getElementsByTagName("TD")[2];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
 }
 
 /*Open the filter pannel and move the screen with it.*/
@@ -624,24 +718,32 @@ function delCard()
 
 function sort(){
   switch(document.getElementsByName("sortBy")[0].value){
-   case "a-z":
-    sortAlphabet();
-    break;
+    case "a-z":
+      sortAlphabet();
+      break;
 
-   case "z-a":
-    sortAlphabetReverse();
-    break;
+    case "z-a":
+      sortAlphabetReverse();
+      break;
 
-   case "dueDate":
-    sortDueDate();
-    break;
+    case "dueDate":
+      sortDueDate();
+      break;
 
-   case "startDate":
-    sortStartDate();
-    break;
+    case "startDate":
+      sortStartDate();
+      break;
 
-   case "status":
-    sortStatus();
-    break;
+    case "category":
+      sortCategory();
+      break;
+
+    case "lastModified":
+      sortLastModified();
+      break;
+
+    case "lastModifiedReversed":
+      sortlastModifiedReversed();
+      break;
   }
 }
